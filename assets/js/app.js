b28a14b2a8702b750fff51140ba88e322deda981
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-01-17 15:27:39 +0530

    page top set

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+// const tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: "#main_section",
+//     pin: true,
+//     start: "top top",
+//     end: "bottom top",
+//     markers: true,
+//     scrub: true,
+//   },
+// });
+
+// tl.fromTo(
+//   ".set_from",
+//   {
+//     y:"150%"
+//   },
+//   {
+//     y:"0"
+//   }
+// );
